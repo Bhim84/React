ef5690d44ac8326c12f910d3db665cb6a79ac2cb
Bhim84 <37284961+Bhim84@users.noreply.github.com>
2018-05-21 08:39:37 +0530

    fetch demo working fine

--- src/fetch.js
+++ src/fetch.js
@@ -1,36 +1,47 @@
 import React, { Component } from "react";
 
 class FetchDemo extends Component {
-constructor(props){
-  super(props);
-  this.state={
-    result:[]
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: [],
+      loading: false
+    };
+    this.fetchDetails = this.fetchDetails.bind(this); 
   }
-  this.fetchDetails=this.fetchDetails.bind(this);
-}
 
-
-fetchDetails(){
-  const myurl ="https://api.github.com/repositories";
-  fetch(myurl)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .then(response => this.setState({result:response}))
-}
+  fetchDetails() {
+    const myurl = "https://api.github.com/repositories";
+    this.setState({ loading: true });
+    fetch(myurl)
+      .then(response => response.json())
+      // .then(response => console.log(response))
+      .then(response => this.setState({ result: response, loading:false }));
+  }
 
   render() {
-    
-    const names=this.state.result.map(function(x){
-      return <li>{x.name}</li>
-    })
+  
+    const names =
+      this.state.loading === true ? (
+        <Loading />
+      ) : (
+        
+          this.state.result.map(function(x,index) {
+          return <li>{x.name}</li>;
+        }) 
+      );
     return (
       <div>
         <h1>Fetch Demo</h1>
-        <input type="button" value="Get Data" onClick={this.fetchDetails}/>
-          {names}
+        <input type="button" value="Get Data" onClick={this.fetchDetails} />
+        {names}
       </div>
     );
   }
 }
-
+const Loading = () => {
+  return (
+    <img alt="dfdsfdsf" src="https://api.optimum7.com/sylvester/deguns/assets/loading.gif" />
+  );
+};
 export default FetchDemo;
